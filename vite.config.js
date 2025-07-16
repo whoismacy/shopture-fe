d1e8bbd2e91ba191c44bfd37baeb8dfdb3e0a906
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2025-07-16 20:08:17 +0300

    Build docker file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+    port: 9090,
+    strictPort: true,
+  },
+  server: {
+    port: 10100,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
