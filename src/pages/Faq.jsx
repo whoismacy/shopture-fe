e40c819b8e1039d97cbd4a8b5cd71783b5947ab9
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2025-09-20 05:56:48 +0300

    Refactor CSS styles and update import paths in Faq component

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,5 +1,5 @@
-import faqData from "../../../public/data/faqs";
-import Accordion from "../../components/ui/Accordion";
+import faqData from "../../public/data/faqs";
+import Accordion from "../components/ui/Accordion";
 
 export default function Faq() {
   return (
