50f862a0f5e7fbab10823309586136c17f0df144
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2025-09-21 15:35:47 +0300

    Update homepage URL and adjust deployment script for Vite build output

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/shopture-fe",
   plugins: [react(), tailwindcss()],
   preview: {
     port: 9090,
